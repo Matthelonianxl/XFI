86503f68b35997d06e40a00f1aa45bb80b8fa477
alistairholt <alistairholt@gmail.com>
2011-12-12 23:56:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# XFI
+
+XFI takes advantage of the HTML5 file input in order to provide a better user experience for multiple file uploads from a single file input.
+
+XFI is for people who want to simply have better file inputs in their forms with support for multiple file uploads. If you're looking for features like drag and drop, asynchronous uploads and progress bars then you might want to try something else.
+
+XFI is a simple jQuery plug-in that can be easily integrated into your existing code.
+
+Project status: **Active**
+
+› [View a demonstration](http://alistairholt.co.uk/projects/xfi/demo)
+› [Read the documentation](http://alistairholt.co.uk/projects/xfi/documentation)
+
+## What XFI provides
+
+- A consistent interface for file inputs across different browsers and platforms that is easily customised via CSS
+- Feedback to the user about selected file(s)
+- Feedback to the user about file uploads taking place on form submission (Optional)
+
+## What XFI doesn't provide
+
+- Drag and drop file uploads
+- Asynchronous file uploads
+- Progress bars
+- File type/size validation
+
+## License
+
+XFI is licensed under a MIT License (See MIT-LICENSE.txt).
+
+## Contributions
+
+Got a bug fix or something to add? Fork and send me a pull request on [GitHub](http://github.com/alistairholt/XFI).
---
+++ jquery.xfi-1.0.js
@@ -0,0 +1,200 @@
+(function ($) {
+  $.XFI = function (options) {
+    var self;
+
+    self = this;
+
+    self.settings = {
+      submitButtonFeeback: true,
+      debug: false
+    };
+
+    $.extend(self.settings, options);
+
+    self.buttonText = "Choose File(s)...";
+
+    self.log = function (message, object) {
+      if (typeof console !== "undefined" && console !== null) {
+        if (typeof object !== "undefined") {
+          console.log("[XFI] " + message + ":");
+          console.log(object);
+        } else {
+          console.log("[XFI] " + message);
+        }
+      }
+    };
+
+    self.generateUIDSegment = function () {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+
+    self.generateUID = function () {
+      return (self.generateUIDSegment() + "-" + self.generateUIDSegment());
+    };
+
+    self.humanizeFileSize = function (size) {
+      var units, i;
+      units = ['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'YB'];
+      i = 0;
+      while (size >= 1024) {
+        size /= 1024;
+        ++i;
+      }
+      return size.toFixed(1) + ' ' + units[i];
+    };
+
+    self.init = function () {
+      if (self.settings.debug === true) {
+        self.log("Initialized");
+      }
+
+      $("form:has(input[type='file'].xfi)").each(function () {
+        var $form, $inputs;
+        $form = $(this);
+        $inputs = $form.find("input[type='file'].xfi");
+        self.log("Found " + $inputs.length + " XFI file input(s)", $inputs);
+        $inputs.each(function () {
+          var $input, inputUID, $proxyInput, $cancel, $fileInfo;
+
+          $input = $(this);
+
+          // Generate an UID for this file input
+          inputUID = self.generateUID();
+
+          // Hide the existing file input
+          // NOTE: We can't just set display to none
+          // because Webkit stops us from proxying
+          // events back to the input
+          $input.css({
+            position: "absolute",
+            top: 0,
+            left: "-1000em",
+            width: 0,
+            height: 0,
+            visibility: "hidden"
+          }).attr("data-xfi", inputUID);
+
+          // Build a proxy file input
+          $proxyInput = $("<button>", {
+            type: "button",
+            "class": "xfi-input",
+            text: self.buttonText
+          }).attr("data-xfi", inputUID);
+
+          // Insert the proxy file input into the DOM
+          $proxyInput.insertAfter($input);
+
+          $cancel = $("<a>", {
+            href: "",
+            "class": "xfi-cancel",
+            "data-xfi": inputUID,
+            text: "Cancel",
+            click: function (e) {
+              var xfiUID, $input, $proxyInput, $fileInfo;
+              xfiUID = $(this).data("xfi");
+              // Reset the value for the original input
+              $input = $("input[data-xfi='" + xfiUID + "'].xfi");
+              $input.val("");
+              // Reset the text for the proxy input
+              $proxyInput = $("button[data-xfi='" + xfiUID + "']");
+              $proxyInput.text(self.buttonText);
+              // Clear the file information
+              $fileInfo = $("ul[data-xfi='" + xfiUID + "']");
+              $fileInfo.empty().hide();
+              $(this).hide();
+              if (self.settings.submitButtonFeeback) {
+                $input.parents("form").unbind(".xfi");
+              }
+              e.preventDefault();
+            }
+          });
+
+          $cancel.insertAfter($proxyInput).hide();
+          $cancel.before(" ");
+
+          // Build a UL to hold file information
+          $fileInfo = $("<ul>").attr("data-xfi", inputUID);
+
+          $fileInfo.insertBefore($proxyInput).hide();
+
+          // Pass click events on the proxy file input
+          // back to the orginal file input
+          $proxyInput.bind("click", function (e) {
+            var $proxyInput, xfiUID, $input;
+            $proxyInput = $(this);
+            xfiUID = $proxyInput.data("xfi");
+            $input = $("input[data-xfi='" + xfiUID + "'].xfi");
+            $input.click();
+
+            e.preventDefault();
+          });
+
+          // Watch the original file input for changes
+          $input.bind("change", function () {
+            var $input, $proxyInput, proxyInputText, xfiUID, $fileInfo, files, $cancel;
+            $input = $(this);
+            xfiUID = $input.data("xfi");
+            $proxyInput = $("button[data-xfi='" + xfiUID + "']");
+            $fileInfo = $("ul[data-xfi='" + xfiUID + "']");
+            $fileInfo.empty().hide();
+            $cancel = $("a[data-xfi='" + xfiUID + "'].xfi-cancel");
+            $cancel.show();
+            files = $input[0].files;
+            if (files.length > 0) {
+              $(files).each(function () {
+                var fileName, fileSize;
+                // Mozilla File attributes
+                //   name, size
+                // WebKit File attributes
+                //   name/fileName, size/fileSize
+                // Opera File attributes
+                //   name, size
+                // Internet Explorer File attributes
+                //   ?
+                self.log("File chosen", this);
+                fileName = this.name;
+                fileSize = this.size;
+                $("<li>", {
+                  html: fileName + " <i>(" + self.humanizeFileSize(fileSize) + ")</i>"
+                }).appendTo($fileInfo);
+              });
+
+              // Show file information
+              $fileInfo.show();
+              // Update proxy input
+              proxyInputText = (files.length > 1) ? " files" : " file";
+
+              $proxyInput.text(files.length + proxyInputText + " chosen. Change?");
+
+              if (self.settings.submitButtonFeeback) {
+                $input.parents("form").bind("submit.xfi", function () {
+                  var $form, $xfiInputs, totalFileCount, fileOrFiles;
+                  $form = $(this);
+                  totalFileCount = 0;
+                  $xfiInputs = $form.find("input.xfi");
+                  $xfiInputs.each(function () {
+                    totalFileCount += this.files.length;
+                  });
+                  if (totalFileCount > 0) {
+                    fileOrFiles = (totalFileCount > 1) ? "files" : "file";
+
+                    $form.find("[type='submit']:last")
+                      .val("Uploading " + totalFileCount + " " + fileOrFiles + "... Please wait...")
+                      .attr("disabled", "disabled");
+                  }
+                });
+              }
+            } else {
+              if (self.settings.submitButtonFeeback) {
+                $input.parents("form").unbind(".xfi");
+              }
+              $proxyInput.text(self.buttonText);
+            }
+          });
+        });
+      });
+    };
+
+    self.init();
+  };
+})(jQuery);
